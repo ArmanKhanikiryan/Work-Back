a7aecd277465cf44e64e0471300661c427478451
ArmanKhanikiryan <akhanikiryan@gmail.com>
2023-03-10 02:11:50 +0400

    arplas bucket configured

--- index.js
+++ index.js
@@ -7,7 +7,23 @@ import multer from "multer";
 import pako from "pako";
 import {Storage} from "@google-cloud/storage";
 
+
+
 (async function () {
+
+
+
+  const storageMainSlider = new Storage({  //MAIN SLIDER KEYS
+    projectId: 'amazing-plateau-380104',
+    keyFilename: './amazing-plateau-380104-1d50eda4af26.json',
+  });
+
+  const storageArplasImages = new Storage({
+    projectId: 'amazing-plateau-380104',
+    keyFilename: './amazing-plateau-380104-91eb04c2e583.json'
+  })
+
+
   const uri = "mongodb+srv://main:93285797@profsystem.prx6imm.mongodb.net/ProfSystem?retryWrites=true&w=majority"; // Proffsystem database
   const mongoClient = new MongoClient(uri, {
     useNewUrlParser: true,
@@ -40,17 +56,16 @@ import {Storage} from "@google-cloud/storage";
   const upload = multer();
 
 
-  const storage = new Storage({
-    projectId: 'amazing-plateau-380104',
-    keyFilename: './amazing-plateau-380104-1d50eda4af26.json',
-  });
+  const bucketName = 'your-bucket-name';
+  const serviceAccount = 'slider@amazing-plateau-380104.iam.gserviceaccount.com';
 
 
+//MAIN SLIDER IMAGES COLLECTION
 
 
-  app.get('/images', async (req, res) => {
+  app.get('/images-main', async (req, res) => {
     try {
-      const [files] = await storage.bucket('proffsystem').getFiles();
+      const [files] = await storageMainSlider.bucket('proffsystem').getFiles();
       const fileUrls = files.map((file) => {
         return {
           name: file.name,
@@ -69,131 +84,177 @@ import {Storage} from "@google-cloud/storage";
 
 
 
-  app.post("/post", upload.single("file"), async (req, res) => {
-    const imageBuffer = Buffer.from(req.file.buffer);
-    const decompressedBuffer = pako.ungzip(imageBuffer);
-    const dataBuffer = Buffer.from(decompressedBuffer);
-    // const image = new Image({
-    //   data: dataBuffer,
-    // });
-    // await image.save();
-    await sliderCollection.insertOne({
-      data: dataBuffer, // use req.file.buffer instead of dataBuffer
-    });
-    res.send("File uploaded successfully!");
-  });
+// ARPLAS IMAGES COLLECTION
+
+  // app.get('/images-arplas', async (req, res) => {
+  //
+  //   try {
+  //     const [files] = await storage.bucket('slider').getFiles();
+  //     const fileUrls = files.map((file) => {
+  //       return {
+  //         name: file.name,
+  //         url: `https://storage.googleapis.com/${file.bucket.name}/${file.name}`,
+  //       };
+  //     });
+  //     res.json(fileUrls);
+  //   } catch (err) {
+  //     console.error(err);
+  //     res.status(500).send('Internal server error');
+  //   }
+  // });
 
 
 
 
-  app.post("/post-arplas", upload.single("file"), async (req, res) => {
 
-    const fullName = req.file.originalname
-    let itemName = ''
-    for (let i = 0; i < fullName.length; i++) {
-      if (fullName[i] === '.'){
-        itemName = fullName.slice(0,i)
-      }
+  app.get('/images-arplas', async (req, res) => {
+    try {
+      const [files] = await storageArplasImages.bucket('slideriimages').getFiles();
+      const fileUrls = files.map((file) => {
+        return {
+          name: file.name,
+          url: `https://storage.googleapis.com/${file.bucket.name}/${file.name}`,
+        };
+      });
+      res.json(fileUrls);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Internal server error');
     }
-    const imageBuffer = Buffer.from(req.file.buffer)
-    const decompressedBuffer = pako.ungzip(imageBuffer)
-    const dataBuffer = Buffer.from(decompressedBuffer)
-    await arplasCollection.insertOne({
-      name: itemName,
-      data: dataBuffer
-    })
-    res.send('Files Uploaded')
   });
 
 
 
 
-
-
-  app.get("/get-test", async (req, res) => {
-
-    // HOW TO GET DATA FROM CERTAIN DATABASE COLLECTION
-
-
-
-    // COMMON WAY TO GET DATA
-
-    // const image = await Image.find();
-    // const resultArr = [];
-    // for (const i in image) {
-    //   const compressedBuffer = pako.gzip(image[i].data);
-    //   console.log(compressedBuffer)
-    //   resultArr.push(compressedBuffer)
-    // }
-    // res.send(resultArr);
-
-  });
+  // app.post("/post", upload.single("file"), async (req, res) => {
+  //   const imageBuffer = Buffer.from(req.file.buffer);
+  //   const decompressedBuffer = pako.ungzip(imageBuffer);
+  //   const dataBuffer = Buffer.from(decompressedBuffer);
+  //   // const image = new Image({
+  //   //   data: dataBuffer,
+  //   // });
+  //   // await image.save();
+  //   await sliderCollection.insertOne({
+  //     data: dataBuffer, // use req.file.buffer instead of dataBuffer
+  //   });
+  //   res.send("File uploaded successfully!");
+  // });
 
 
 
-  app.get("/get:id", async (req, res) => {
 
-    // if (+req.params.id === 1){
-    //   const readyData = await generateGet(sliderCollection, {
-    //     isCompletely: false,
-    //     arrayStart: 0,
-    //     arrayEnd: 2,
-    //     additionalInfo: false
-    //   })
-    //   res.send(readyData)
-    // }else if (+req.params.id === 2){
-    //   const readyData = await generateGet(sliderCollection, {
-    //     isCompletely: false,
-    //     arrayStart: 2,
-    //     arrayEnd: 4,
-    //     additionalInfo: false
-    //   })
-    //   res.send(readyData)
-    // }
-
-    // generateGet(sliderCollection, {
-    //   isCompletely: false,
-    //   arrayStart: 0,
-    //   arrayEnd: 2
-    // })
-
-    // HOW TO GET DATA FROM CERTAIN DATABASE COLLECTION
-
-    if (+req.params.id === 1){
-      const image = await sliderCollection.find().toArray()
-      const resultArr = [];
-      for (let i = 0; i < 2; i++) {
-        const binaryData = image[i].data;
-        const bufferData = Buffer.from(binaryData.buffer, binaryData.byteOffset, binaryData.length);
-        const compressedBuffer = pako.gzip(bufferData);
-        resultArr.push(compressedBuffer)
-      }
-      res.send(resultArr);
-    }else if (+req.params.id === 2){
-      const image = await sliderCollection.find().toArray()
-      const resultArr = [];
-      for (let i = 2; i < 4; i++) {
-        const binaryData = image[i].data;
-        const bufferData = Buffer.from(binaryData.buffer, binaryData.byteOffset, binaryData.length);
-        const compressedBuffer = pako.gzip(bufferData);
-        resultArr.push(compressedBuffer)
-      }
-      res.send(resultArr);
-    }
-
-    // COMMON WAY TO GET DATA
-
-    // const image = await Image.find();
-    // const resultArr = [];
-    // for (const i in image) {
-    //   const compressedBuffer = pako.gzip(image[i].data);
-    //   console.log(compressedBuffer)
-    //   resultArr.push(compressedBuffer)
-    // }
-    // res.send(resultArr);
-
-  });
+  // app.post("/post-arplas", upload.single("file"), async (req, res) => {
+  //
+  //   const fullName = req.file.originalname
+  //   let itemName = ''
+  //   for (let i = 0; i < fullName.length; i++) {
+  //     if (fullName[i] === '.'){
+  //       itemName = fullName.slice(0,i)
+  //     }
+  //   }
+  //   const imageBuffer = Buffer.from(req.file.buffer)
+  //   const decompressedBuffer = pako.ungzip(imageBuffer)
+  //   const dataBuffer = Buffer.from(decompressedBuffer)
+  //   await arplasCollection.insertOne({
+  //     name: itemName,
+  //     data: dataBuffer
+  //   })
+  //   res.send('Files Uploaded')
+  // });
+  //
+  //
+  //
+  //
+  //
+  //
+  // app.get("/get-test", async (req, res) => {
+  //
+  //   // HOW TO GET DATA FROM CERTAIN DATABASE COLLECTION
+  //
+  //
+  //
+  //   // COMMON WAY TO GET DATA
+  //
+  //   // const image = await Image.find();
+  //   // const resultArr = [];
+  //   // for (const i in image) {
+  //   //   const compressedBuffer = pako.gzip(image[i].data);
+  //   //   console.log(compressedBuffer)
+  //   //   resultArr.push(compressedBuffer)
+  //   // }
+  //   // res.send(resultArr);
+  //
+  // });
+  //
+  //
+  //
+  // app.get("/get:id", async (req, res) => {
+  //
+  //   // if (+req.params.id === 1){
+  //   //   const readyData = await generateGet(sliderCollection, {
+  //   //     isCompletely: false,
+  //   //     arrayStart: 0,
+  //   //     arrayEnd: 2,
+  //   //     additionalInfo: false
+  //   //   })
+  //   //   res.send(readyData)
+  //   // }else if (+req.params.id === 2){
+  //   //   const readyData = await generateGet(sliderCollection, {
+  //   //     isCompletely: false,
+  //   //     arrayStart: 2,
+  //   //     arrayEnd: 4,
+  //   //     additionalInfo: false
+  //   //   })
+  //   //   res.send(readyData)
+  //   // }
+  //
+  //   // generateGet(sliderCollection, {
+  //   //   isCompletely: false,
+  //   //   arrayStart: 0,
+  //   //   arrayEnd: 2
+  //   // })
+  //
+  //   // HOW TO GET DATA FROM CERTAIN DATABASE COLLECTION
+  //
+  //   if (+req.params.id === 1){
+  //     const image = await sliderCollection.find().toArray()
+  //     const resultArr = [];
+  //     for (let i = 0; i < 2; i++) {
+  //       const binaryData = image[i].data;
+  //       const bufferData = Buffer.from(binaryData.buffer, binaryData.byteOffset, binaryData.length);
+  //       const compressedBuffer = pako.gzip(bufferData);
+  //       resultArr.push(compressedBuffer)
+  //     }
+  //     res.send(resultArr);
+  //   }else if (+req.params.id === 2){
+  //     const image = await sliderCollection.find().toArray()
+  //     const resultArr = [];
+  //     for (let i = 2; i < 4; i++) {
+  //       const binaryData = image[i].data;
+  //       const bufferData = Buffer.from(binaryData.buffer, binaryData.byteOffset, binaryData.length);
+  //       const compressedBuffer = pako.gzip(bufferData);
+  //       resultArr.push(compressedBuffer)
+  //     }
+  //     res.send(resultArr);
+  //   }
+  //
+  //   // COMMON WAY TO GET DATA
+  //
+  //   // const image = await Image.find();
+  //   // const resultArr = [];
+  //   // for (const i in image) {
+  //   //   const compressedBuffer = pako.gzip(image[i].data);
+  //   //   console.log(compressedBuffer)
+  //   //   resultArr.push(compressedBuffer)
+  //   // }
+  //   // res.send(resultArr);
+  //
+  // });
 
   app.listen(3333);
 })();
+
+
+
+
 
