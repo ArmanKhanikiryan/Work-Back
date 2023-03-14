5129ece0257d8d2b5495d17b2f0d968f3f63d71f
Arman Khanikiryan <akhanikiryan@gmail.com>
2023-03-15 00:37:02 +0400

    arplas images dnoe

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
 import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3";
+import sharp from "sharp";
+
+
 
 
 (async function () {
-
   const app = express();
+  const cacheTime = 86400000; // 1 day in milliseconds
+
+  app.use("/public", express.static("public", { maxAge: cacheTime }));
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   app.use(express.static("public/images/test.png"));
@@ -24,6 +29,20 @@ import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3";
     },
   });
 
+  // const s3Arplas = new S3Client({
+  //   region: "us-east-2",
+  //   credentials: {
+  //     accessKeyId: "AKIA5OJVOLKBBYFVOOHG",
+  //     secretAccessKey: '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',
+  //   },
+  // });
+
+
+
+  const compressImage = async (url) => {
+    const image = await sharp(url).webp({ quality: 80 }).toBuffer();
+    return `data:image/jpeg;base64,${image.toString('base64')}`;
+  }
 
   app.get('/images-main', async (req, res) => {
     try {
@@ -33,7 +52,25 @@ import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3";
         name: file.Key,
         url: `https://s3.us-east-2.amazonaws.com/${response.Name}/${file.Key}`,
       }));
-      res.json(fileUrls);
+      const compressedUrls = await Promise.all(fileUrls);
+      res.json(compressedUrls);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Internal server error');
+    }
+  });
+
+
+  app.get('/images-arplas', async (req, res) => {
+    try {
+      const command = new ListObjectsV2Command({ Bucket: "arplas" });
+      const response = await s3.send(command);
+      const fileUrls = response.Contents.map((file) => ({
+        name: file.Key,
+        url: `https://s3.us-east-2.amazonaws.com/${response.Name}/${file.Key}`,
+      }));
+      const compressedUrls = await Promise.all(fileUrls);
+      res.json(compressedUrls);
     } catch (err) {
       console.error(err);
       res.status(500).send('Internal server error');
