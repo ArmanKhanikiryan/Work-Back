9f76b39db1d1c041195b94aa1f4e496675e373a6
ArmanKhanikiryan <akhanikiryan@gmail.com>
2023-03-14 23:42:59 +0400

    test

--- index.js
+++ index.js
@@ -1,94 +1,20 @@
-// import bodyParser from "body-parser";
-// import cors from "cors";
-// import express from "express";
-// import { MongoClient, ServerApiVersion } from "mongodb";
-// import mongoose, { Schema } from "mongoose";
-// import AWS from 'aws-sdk';
-//
-//
-//
-//
-//
-// (async function () {
-//
-//
-//   const uri = "mongodb+srv://main:93285797@profsystem.prx6imm.mongodb.net/ProfSystem?retryWrites=true&w=majority"; // Proffsystem database
-//   const mongoClient = new MongoClient(uri, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     serverApi: ServerApiVersion.v1,
-//   });
-//   await mongoose.connect(uri);
-//   await mongoClient.connect();
-//   mongoose.set("strictQuery", false);
-//   const app = express();
-//   app.use(express.urlencoded({ extended: true }));
-//   app.use(express.json());
-//   app.use(express.static("public/images/test.png"));
-//   app.use(cors());
-//   app.use(bodyParser.json({ limit: "50mb" }));
-//   app.use(bodyParser.urlencoded({ extended: true }));
-//   app.use(bodyParser.json());
-//   app.set("serverMaxHeaderSize", 1024 * 4096);
-//
-//
-//
-//
-//   const s3 = new AWS.S3({
-//     accessKeyId: process.env.AWS_ACCESSKEY,
-//     secretAccessKey: process.env.AWS_SECRETKEY,
-//   });
-//
-//   app.get('/images-main', async (req, res) => {
-//     try {
-//       const data = await s3.listObjectsV2({ Bucket: 'proffsystem' }).promise();
-//       const fileUrls = data.Contents.map((file) => ({
-//         name: file.Key,
-//         url: `https://s3.amazonaws.com/${data.Name}/${file.Key}`,
-//       }));
-//       res.json(fileUrls);
-//     } catch (err) {
-//       console.error(err);
-//       res.status(500).send('Internal server error');
-//     }
-//   });
-//
-//
-//
-//
-//   app.listen(3333);
-// })();
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3";
 
-
-import https from 'https';
-import fs from 'fs';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import express from 'express';
-import { MongoClient, ServerApiVersion } from 'mongodb';
-import mongoose, { Schema } from 'mongoose';
-import { S3Client, ListObjectsV2Command } from '@aws-sdk/client-s3';
-import * as path from "path";
 
 (async function () {
-  const uri = 'mongodb+srv://main:93285797@profsystem.prx6imm.mongodb.net/ProfSystem?retryWrites=true&w=majority'; // Proffsystem database
-  const mongoClient = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverApi: ServerApiVersion.v1,
-  });
-  await mongoose.connect(uri);
-  await mongoClient.connect();
-  mongoose.set('strictQuery', false);
+
   const app = express();
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-  app.use(express.static('public/images/test.png'));
+  app.use(express.static("public/images/test.png"));
   app.use(cors());
-  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.json({ limit: "50mb" }));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
-  app.set('serverMaxHeaderSize', 1024 * 4096);
+  app.set("serverMaxHeaderSize", 1024 * 4096);
 
   const s3 = new S3Client({
     region: "us-east-2",
@@ -98,15 +24,8 @@ import * as path from "path";
     },
   });
 
-  app.use((req, res, next) => {
-    console.log(`Incoming request: ${req.method} ${req.url}`);
-    next();
-  });
-
-
 
   app.get('/images-main', async (req, res) => {
-    console.log('get')
     try {
       const command = new ListObjectsV2Command({ Bucket: "proffsystem" });
       const response = await s3.send(command);
@@ -121,15 +40,8 @@ import * as path from "path";
     }
   });
 
-  const certPath = path.resolve('server.crt');
-  const keyPath = path.resolve('server.key');
 
-  const options = {
-    cert: fs.readFileSync(certPath),
-    key: fs.readFileSync(keyPath)
-  };
-
-  https.createServer(options, app).listen(3333);
+  app.listen(3333);
 })();
 
 
